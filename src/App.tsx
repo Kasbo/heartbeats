b08ac00bc25ec77b17525ff247d1c603507b55d7
Pavel Piseckiy <p.piseckiy@exaft.com>
2020-02-07 00:49:51 +0300

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import moment from 'moment';
 import './App.css';
 
-const App = () => {
+
+const DATE_WE_MET = moment('11/11/2016 18:00:00', 'DD/MM/YYYY hh:mm:dd');
+
+type BeatsProps = {
+  shouldVibrate: boolean;
+}
+
+function Beats({shouldVibrate} : BeatsProps): JSX.Element {
+  const [beats, setBeats] = React.useState(
+    moment().diff(DATE_WE_MET, 'seconds')
+  );
+
+  const getBeats = React.useCallback((): number => {
+    return moment().diff(DATE_WE_MET, 'seconds');
+  }, []);
+
+  React.useEffect((): (() => void) => {
+    const interval = setInterval((): void => {
+      const nextBeats = getBeats();
+      setBeats(nextBeats);
+
+      if (shouldVibrate) {
+        window.navigator.vibrate(200);
+        console.log('vibrating');
+      }
+    }, 1000);
+
+    return (): void => {
+      clearInterval(interval);
+      navigator.vibrate(0);
+    };
+  }, [getBeats, shouldVibrate]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="beats">
+      {beats} раз
+    </div>
+  );
+}
+
+function App() {
+  const [shouldVibrate, setShouldVibrate] = React.useState(false);
+
+  const toggleShouldVibrate = () => {
+    setShouldVibrate(prevState => !prevState);
+    window.navigator.vibrate(100);
+  };
+
+  return (
+    <div className="content">
+      <div className="title">Привет, Аня</div>
+      <div className="heart-wrapper">
+        <div
+          className="heart"
+          onClick={toggleShouldVibrate}
+        />
+      </div>
+      <div className="beats-label">
+        Сейчас моё сердце бьется для тебя в
+      </div>
+      <Beats shouldVibrate={shouldVibrate}/>
     </div>
   );
 }
